67e6a42fb76de477bcdfab2fe9e174faf6d1de66
Arefin <ahmedarefinsajjad@gmail.com>
2023-01-27 12:02:02 +0600

    mojito: Proton bringup

--- mojito.mk
+++ mojito.mk
@@ -11,22 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Proton stuff.
+$(call inherit-product, vendor/proton/config/common.mk)
+PROTON_BUILD_VARIANT := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_mojito
+PRODUCT_NAME := mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# ArrowOS additions
-DEVICE_MAINTAINER := sarthakroy2002, blur
-TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Build
 BUILD_FINGERPRINT := "Redmi/mojito/mojito:12/RKQ1.210614.002/V13.0.11.0.SKGMIXM:user/release-keys"
